851f201839c72286b9f18da583179bc8d9358f86
Hilme <hilmetadese@gmail.com>
2022-12-29 05:22:31 -0800

    h.py file created by paul

---
+++ h.py
@@ -0,0 +1,25 @@
+def sum():
+    print("hello class!")
+    '''hello aymi '''
+sum()          
+x,y,z = 1,2,3
+print(x)
+hilme = "fhtrhtdgdgfgsfsf"
+print(y)
+print(z)
+x=y=z=8
+print (x)
+print(y)
+print(z)
+print(type(hilme))
+print(type(x))
+print(input("Enter two number"))
+x= input("enter first number")
+y= input("enter second number")
+sum = int(x) + int(y)
+print(sum)
+b="blen"
+a="Aymi"
+m="mahi"
+e="ema"
+print(f"i miss {b},{a},{m} and {e} alot")
